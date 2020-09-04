ba68c71434de5adfb0838455507799475e91820e
Edward-Shawn <sbh3834@naver.com>
2020-09-04 18:25:32 +0900

    콘텐츠 페이지 전환 기능 구현

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,50 @@
 $(window).ready(function(){
+
+    $(".click").click(function(){
+        $(".click").removeClass("active");
+        $(this).addClass("active");
+
+        if($(".click.one").hasClass("active")){
+            $(".view-all").css('display','none')
+            $(".pages").css('display','block')
+        }
+
+        if($(".click.all").hasClass("active")){
+            $(".pages").css('display','none')
+            $(".view-all").css('display','block')
+        }
+    })
+
     
+
+
+
+
+    $('.grid').isotope({
+        // options
+        itemSelector: '.grid-item',
+        layoutMode: 'fitRows'
+      });
+
+
+    var $grid = $('.grid').isotope({
+        // options
+    });
+    // filter items on button click
+    $('.filter-button-group').on( 'click', 'button', function() {
+        var filterValue = $(this).attr('data-filter');
+        $grid.isotope({ filter: filterValue });
+    });
+
+
+
+    // filter .metal items
+    $grid.isotope({ filter: '.one' });
+
+    $grid.isotope({ filter: '.two' });
+
+    $grid.isotope({ filter: '.three' });
+
+    // show all items
+    $grid.isotope({ filter: '*' });
 })
